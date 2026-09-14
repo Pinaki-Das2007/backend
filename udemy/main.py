30100b048c7ec3457b3bcae338a8a83c067c87ca
Pinaki-Das2007 <daspinaki151@gmail.com>
2026-09-14 21:28:17 +0530

    updated the main py file

--- udemy/main.py
+++ udemy/main.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask,render_template
 
 app = Flask(__name__)
 
@@ -6,9 +6,13 @@ app = Flask(__name__)
 def welcome():
     return "<html><h1>Welcome to the Flask application</h1></html>" 
 
-@app.route("/index ")
+@app.route("/index")
 def index():
-    return "This is a index page to showcase the backend turn."
+    return render_template("index.html")
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
 
 if __name__ == "__main__":
     app.run(debug = True)
