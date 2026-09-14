42d910e0fee66a8a92e4f2edcccd75803d7f1e78
Pinaki-Das2007 <daspinaki151@gmail.com>
2026-09-14 21:25:32 +0530

    commented the app files

--- udemy/app.py
+++ udemy/app.py
@@ -1,18 +1,18 @@
-from flask import Flask
-'''
-It creates an instance of the Flask class,
-which will be your WSGI (web server gateway interface ) application.
-'''
-### WSGI Application
-app = Flask(__name__)
+# from flask import Flask
+# '''
+# It creates an instance of the Flask class,
+# which will be your WSGI (web server gateway interface ) application.
+# '''
+# ### WSGI Application
+# app = Flask(__name__)
 
-@app.route("/")
-def welcome():
-    return "Welcome to this Flask application! This should be an amazing experience for you. We will try to make it as simple as possible for everyone.Thank you for visiting us."
+# @app.route("/")
+# def welcome():
+#     return "Welcome to this Flask application! This should be an amazing experience for you. We will try to make it as simple as possible for everyone.Thank you for visiting us."
 
-@app.route("/index")
-def index():
-    return "This is a index page to show case the backend turn."
+# @app.route("/index")
+# def index():
+#     return "This is a index page to show case the backend turn."
 
-if __name__ == "__main__":
-    app.run()
+# if __name__ == "__main__":
+#     app.run()
