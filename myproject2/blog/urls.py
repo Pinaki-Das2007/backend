f6a9bd9a3d31b6b4f7775d9a164d631d3c60bd43
Pinaki-Das2007 <daspinaki151@gmail.com>
2026-09-19 20:18:53 +0530

    myproject2 started

---
+++ myproject2/blog/urls.py
@@ -0,0 +1,7 @@
+from . import views
+from django.urls import path
+
+urlspatterns=[
+    path("post/<int:post_id>/",views.post_details,name="post_details"),
+    path("user/<str:username>/",views.user_profile,name="user_profile"),
+]
